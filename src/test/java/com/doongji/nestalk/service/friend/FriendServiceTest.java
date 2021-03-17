1838c03823d72d0a208efed756aacff23cb2becd
hyojin107 <hyojin107@naver.com>
2021-03-18 02:11:22 +0900

    refactor : 친구등록 테스트 코드 수정

--- src/test/java/com/doongji/nestalk/service/friend/FriendServiceTest.java
+++ src/test/java/com/doongji/nestalk/service/friend/FriendServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,33 +36,22 @@ class FriendServiceTest {
 
     private String phone;
 
-    private LocalDate birthday;
-
-    private User user;
-
-    @BeforeAll
-    void setup() {
-        friendEmail = "newdoongji.team@naver.com";
-        name = "둥지";
-        email = "doongji.team@gmail.com";
-        password = "P@ssword1";
-        phone = "010-0000-0000";
-        birthday = LocalDate.of(1995, 2, 19);
-    }
+    private Long userId = 1L;
 
     @Test
     @Order(1)
     void 사용자_회원가입() {
-        this.user = userService.join(email, name, password, phone, birthday);
+        userService.join("doongji.team@gmail.com", "둥지", "P@ssword1", "010-0000-0000", LocalDate.of(1995, 10, 7));
+        userService.join("newdoongji.team@naver.com", "친구친구", "P@ssword1", "010-1111-0000", LocalDate.of(1995, 2, 19));
     }
 
     @Test
     @Order(2)
     void 친구_등록() {
-        Friend friend = friendService.register(user.getUserId(), friendEmail);
+        Friend friend = friendService.register(1L, "newdoongji.team@naver.com");
         assertThat(friend).isNotNull();
-        //assertThat(friend.getMe().getUserId()).isEqualTo(user.getUserId());
-        //assertThat(friend.getFriend()).isEqualTo(friendEmail);
+        assertThat(friend.getMe().getUserId()).isEqualTo(1L);
+        assertThat(friend.getFriend().getEmail()).isEqualTo("newdoongji.team@naver.com");
         log.info("Friend: {}", friend);
     }
 
