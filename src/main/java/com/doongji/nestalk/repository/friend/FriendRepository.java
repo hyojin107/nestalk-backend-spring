11619f26fe1dbc4bf9561a97262f4d1d0dd10bbc
hyojin107 <hyojin107@naver.com>
2021-03-27 01:36:12 +0900

    feat : Friend 객체 조회 추가

--- src/main/java/com/doongji/nestalk/repository/friend/FriendRepository.java
+++ src/main/java/com/doongji/nestalk/repository/friend/FriendRepository.java
@@ -1,9 +1,13 @@
 package com.doongji.nestalk.repository.friend;
 
 import com.doongji.nestalk.entity.friend.Friend;
+import com.doongji.nestalk.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+
+    Optional<Friend> findByMeAndFriend(User me, User friend);
 
 }
